276e3f57eb844e5f96a78765c40c86c3aa933af9
Kaoru Kyuno <Kaoru.Kyuno@nearline.net>
2020-06-16 21:53:30 +0900

    Test framework

---
+++ TestFramework/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  TestFramework
+//
+//  Created by Kaoru Kyuno on 2020/06/16.
+//  Copyright © 2020 Kaoru Kyuno. All rights reserved.
+//
+
+import SwiftGRPC
+import Foundation
+
+class Hoge {
+    
+    func a() -> Void {
+        gRPC.initialize();
+    }
+    
+}
